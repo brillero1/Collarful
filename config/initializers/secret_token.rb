2b458855e21fe6f410c4f0d25d01f824bd21f236
Omer Briller <brillero@gmail.com>
2014-01-08 10:29:07 +0200

    first time commiting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Collarful::Application.config.secret_key_base = '70d27cdd69f6c981e5e85ec7d0e62313bb23aabbe56a5899382554816a688cc833520c2d8612f6352c9d75a398b2404db1e0656841b5619fa25830937f986722'
